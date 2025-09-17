91ff6e115b2657719515909b10d4580b706575ab
William <estevamwilliam16@gmail.com>
2025-09-16 21:14:30 -0300

    :sparkles: feat: Creating the main components and registering the Teacher component listing

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Home } from './home/home';
+import { Teacher } from './teacher/teacher';
+import { NavBar } from './nav-bar/nav-bar';
+import { Footer } from './footer/footer';
+import { Subjects } from './subjects/subjects';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Home,
+    Teacher,
+    NavBar,
+    Footer,
+    Subjects
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Teacher } from './teacher/teacher';
+import { Subjects } from './subjects/subjects';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {path: '', component: Home},
+    {path: 'teacher', component: Teacher},
+    {path: 'subjects', component: Subjects}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
